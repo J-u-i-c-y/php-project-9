4e44ab112f8df9aef3554513f929ebf49ae33cc2
Andrei Goncharenko <andrei.h@seranking.com>
2025-10-03 09:37:48 +0200

    add first test

--- public/index.php
+++ public/index.php
@@ -3,7 +3,6 @@
 use Slim\Factory\AppFactory;
 use Slim\Middleware\MethodOverrideMiddleware;
 use DI\Container;
-use Dotenv\Dotenv;
 use Illuminate\Support\Collection;
 use Hexlet\Code\Url;
 use Hexlet\Code\UrlRepo;
@@ -14,44 +13,46 @@ use Hexlet\Code\CheckRepo;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
-$dotenv->load();
-
 session_start();
 
 $container = new Container();
-$container->set('renderer', function () {
+$container->set(
+    'renderer',
+    function () {
         return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
     }
 );
+$container->set(
+    'flash',
+    function () {
+        return new \Slim\Flash\Messages();
+    }
+);
 
-$container->set('flash', function () {
-    return new \Slim\Flash\Messages();
-});
-
-$container->set(\PDO::class, function () {
+$container->set(
+    \PDO::class,
+    function () {
         $databaseUrl = parse_url($_ENV['DATABASE_URL']);
 
-        $user = $databaseUrl['user'];
-        $pass = $databaseUrl['pass'];
         $host = $databaseUrl['host'];
-        $port = $databaseUrl['port'];
-        $dbName = ltrim($databaseUrl['path'], '/');
+        $port = $databaseUrl['port'] ?? '5432';
+        $dbname = ltrim($databaseUrl['path'], '/');
+        $user = $databaseUrl['user'];
+        $password = $databaseUrl['pass'];
         $conStr = sprintf(
             "pgsql:host=%s;port=%d;dbname=%s;user=%s;password=%s",
             $host,
             $port,
-            $dbName,
+            $dbname,
             $user,
-            $pass
+            $password
         );
-
-        $conn = new PDO($conStr, $user, $pass);
-        $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $conn = new \PDO($conStr);
+        $conn->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
         return $conn;
     }
 );
+
 
 $initFilePath = implode('/', [dirname(__DIR__), 'database.sql']);
 $initSql = file_get_contents($initFilePath);
@@ -59,26 +60,24 @@ if ($initSql) {
     $container->get(\PDO::class)->exec($initSql);
 }
 
+
 $app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
 $app->add(MethodOverrideMiddleware::class);
 $router = $app->getRouteCollector()->getRouteParser();
 
-// Главная страница с формой
-$app->get('/', function ($request, $response) {
-        return $this->get('renderer')->render($response, 'index.phtml');
+$app->get(
+    '/',
+    function ($req, $res) {
+        return $this->get('renderer')->render($res, 'index.phtml');
     }
 )->setName('urls.index');
 
-$app->post('/urls', function ($request, $response) use ($router) {
+$app->post(
+    '/urls',
+    function ($req, $res) use ($router) {
         $urlRepo = $this->get(UrlRepo::class);
-        // $urlData = $request->getParsedBodyParam('url');
-
-        // $userData = $request->getParsedBodyParam('user');
-        $parsedBody = $request->getParsedBody();
-        $urlData = $parsedBody['url'] ?? [];
-
-
+        $urlData = $req->getParsedBodyParam('url');
         $validator = new UrlValidator();
         $errors = $validator->validate($urlData);
 
@@ -91,18 +90,14 @@ $app->post('/urls', function ($request, $response) use ($router) {
                 $id = $url->getId();
                 $this->get('flash')->addMessage('success', 'Страница уже существует');
 
-                return $response
-                        ->withHeader('Location', $router->urlFor('urls.show', ['id' => $id]))
-                        ->withStatus(302);
+                return $res->withRedirect($router->urlFor('urls.show', ['id' => "$id"]));
             }
 
             $urlRepo->save($url);
             $id = $url->getId();
             $this->get('flash')->addMessage('success', 'Страница успешно добавлена');
 
-            return $response
-                        ->withHeader('Location', $router->urlFor('urls.show', ['id' => $id]))
-                        ->withStatus(302);
+            return $res->withRedirect($router->urlFor('urls.show', ['id' => "$id"]));
         }
 
         $params = [
@@ -110,28 +105,30 @@ $app->post('/urls', function ($request, $response) use ($router) {
             'errors' => $errors
         ];
 
-        return $this->get('renderer')->render($response->withStatus(422), 'index.phtml', $params);
+        return $this->get('renderer')->render($res->withStatus(422), 'index.phtml', $params);
     }
 );
 
-// Страница со списком URL-ов
-$app->get('/urls', function ($request, $response) {
+$app->get(
+    '/urls',
+    function ($req, $res) {
         $urlRepo = $this->get(UrlRepo::class);
         $urls = $urlRepo->getEntities();
         $params = ['urls' => $urls];
 
-        return $this->get('renderer')->render($response, 'store.phtml', $params);
+        return $this->get('renderer')->render($res, 'store.phtml', $params);
     }
 )->setName('urls.store');
 
-// Страница одного URL по ID
-$app->get('/urls/{id}', function ($request, $response, $args) {
+$app->get(
+    '/urls/{id}',
+    function ($req, $res, $args) {
         $urlRepo = $this->get(UrlRepo::class);
         $id = $args['id'];
         $url = $urlRepo->find($id);
 
         if (is_null($url)) {
-            return $this->get('renderer')->render($response->withStatus(404), '404.phtml');
+            return $this->get('renderer')->render($res->withStatus(404), '404.phtml');
         }
 
         $checkRepo = $this->get(CheckRepo::class);
@@ -139,12 +136,13 @@ $app->get('/urls/{id}', function ($request, $response, $args) {
         $flash = $this->get('flash')->getMessages();
         $params = ['url' => $url, 'flash' => $flash, 'checks' => $checks];
 
-        return $this->get('renderer')->render($response, 'show.phtml', $params);
+        return $this->get('renderer')->render($res, 'show.phtml', $params);
     }
-
 )->setName('urls.show');
 
-$app->post('/urls/{url_id}/checks', function ($request, $response, $args) use ($router) {
+$app->post(
+    '/urls/{url_id}/checks',
+    function ($req, $res, $args) use ($router) {
         $urlId = $args['url_id'];
         $check = Check::fromArray([$urlId]);
         $checkRepo = $this->get(CheckRepo::class);
@@ -154,15 +152,15 @@ $app->post('/urls/{url_id}/checks', function ($request, $response, $args) use ($
 
         if (is_null($checkWithRequestStatus)) {
             $this->get('flash')->addMessage('error', 'Произошла ошибка при проверке, не удалось подключиться');
-        } else {
-            $parsedCheck = $checkWithRequestStatus->parseHtml($url->getName());
-            $checkRepo->save($parsedCheck);
-            $this->get('flash')->addMessage('success', 'Страница успешно проверена');
+
+            return $res->withRedirect($router->urlFor('urls.show', ['id' => $urlId]));
         }
 
-        return $response
-            ->withHeader('Location', $router->urlFor('urls.show', ['id' => $urlId]))
-            ->withStatus(302);
+        $parsedCheck = $checkWithRequestStatus->parseHtml($url->getName());
+        $checkRepo->save($parsedCheck);
+        $this->get('flash')->addMessage('success', 'Страница успешно проверена');
+
+        return $res->withRedirect($router->urlFor('urls.show', ['id' => $urlId]));
     }
 );
 
